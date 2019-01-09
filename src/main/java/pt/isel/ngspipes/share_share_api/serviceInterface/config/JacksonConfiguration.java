95eef67ec73cd6d9e15e9b1b06ed6f22a754ceca
BrunoMNDantas <bmndantas@gmail.com>
2019-01-09 18:10:52 +0000

    Mapper ignoring unknown properties

--- src/main/java/pt/isel/ngspipes/share_share_api/serviceInterface/config/JacksonConfiguration.java
+++ src/main/java/pt/isel/ngspipes/share_share_api/serviceInterface/config/JacksonConfiguration.java
@@ -1,5 +1,6 @@
 package pt.isel.ngspipes.share_share_api.serviceInterface.config;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import org.springframework.context.annotation.Bean;
@@ -25,6 +26,8 @@ public class JacksonConfiguration {
         mapper.registerModule(pipelineSerializerModule);
         mapper.registerModule(pipelineDeserializerModule);
         mapper.registerModule(toolTypeResolverModule);
+
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 
         return mapper;
     }
